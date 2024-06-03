dba749e5e21b8041a762c4a04c9754c85673fad2
yohannistk <onlinejobs2116@gmail.com>
2024-06-03 17:36:43 +0300

    bug fixed in component chat-list

--- app/(dashboard)/components/chat-list.tsx
+++ app/(dashboard)/components/chat-list.tsx
@@ -3,8 +3,8 @@ import Chat from "./chat";
 const ChatList = () => {
   return (
     <div className="p-2 hover: overflow-y-auto space-y-3">
-      {new Array(20).fill("").map((chat) => {
-        return <Chat />;
+      {new Array(20).fill("").map((chat, index) => {
+        return <Chat key={index} />;
       })}
     </div>
   );
